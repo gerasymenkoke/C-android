8fc14a1a884e8a94922e78f469d54a6ed305c154
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2022-07-01 08:09:14 +0300

    Update test.c

--- test.c
+++ test.c
@@ -19,4 +19,28 @@
 #include "CNFG.h"
 
 
+#include <stdio.h>
+#include<stdlib.h>
+
+int main(int quantity, char  **number) {    
+
+     int  sum;
+    
+    // printf("Enter two integers: ");
+    
+    // scanf("%d %d", &number1, &number2);
+
+    // calculating sum
+    // sum = number1 + number2;      
+    sum=1;
+
+    // printf("%s %s", number[1], number[2]);
+    int num1 = atoi(number[1]); 
+    int num2 = atoi(number[2]); 
+    sum = num1 + num2;
+    printf("%d + %d = %d", num1, num2, sum);
+    
+    return 0;
+}
+
 	
