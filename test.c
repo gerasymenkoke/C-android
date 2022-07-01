f224356e1e25356d26092e8d1aba6c64eb1776ef
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2022-07-01 08:05:43 +0300

    Create test.c

---
+++ test.c
@@ -0,0 +1,22 @@
+//Copyright (c) 2011-2020 <>< Charles Lohr - Under the MIT/x11 or NewBSD License you choose.
+// NO WARRANTY! NO GUARANTEE OF SUPPORT! USE AT YOUR OWN RISK
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "os_generic.h"
+#include <GLES3/gl3.h>
+#include <asset_manager.h>
+#include <asset_manager_jni.h>
+#include <android_native_app_glue.h>
+#include <android/sensor.h>
+#include "CNFGAndroid.h"
+
+#define CNFG_IMPLEMENTATION
+#define CNFG3D
+
+#include "CNFG.h"
+
+
+	
